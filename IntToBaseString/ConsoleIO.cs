bea1cf29068832a53db2ee98419c365eda0af2bc
VBTCSE <jhinckley@vbisd.org>
2018-02-05 05:35:17 -0500

    Added C# project.

---
+++ IntToBaseString/ConsoleIO.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntToBaseString
+{
+    class ConsoleIO
+    {
+        public static int getNumber(int minVal = 0, int maxVal = 0)
+        {
+            // Input a 32-bit integer >= minVal from the user.  The default value of minVal is 0.
+            // Return this value.
+
+
+            int n = -1;  // Initial, default return value.  Returning -1 signals an error in this method.
+
+            if (minVal == 0)
+            {
+                // Ask the user to enter a non-negative integer.
+                // Reference: https://msdn.microsoft.com/en-us/library/system.console.write(v=vs.110).aspx
+                Console.Write("Enter a non-negative integer: ");
+            }
+            else
+            {
+                // Ask the user to enter an integer >= minVal.
+                Console.Write("Enter an integer >= {0}: ", minVal);
+            }
+
+
+
+
+            if (maxVal != 0)
+            {
+                // A value of maxVal = 0, is a flag, indicating that there is no upper limit on the user's input.
+                // If maxVal is anything else, then the user's input must be <= maxVal (assuming that maxVal >= minVal).
+
+                // First, verify that maxVal >= minVal.  Calling program could make an incorrect call...
+                if (maxVal < minVal)
+                {
+                    try
+                    {
+                        throw new ArgumentException(String.Format("Invalid argument pair in getNumber(): maxVal = {0} < minVal = {1}.", maxVal, minVal));
+                    }
+                    catch (ArgumentException e)
+                    {
+                        Console.WriteLine(e.Message);   // Output an error message if arguments are invalid.
+                        return n;   // Return the value n = -1, to signal an error.
+                    }
+                }
+            }
+
+
+
+
+            // Start a do-while loop to ensure that the input integer is >= minVal.
+            do
+            {
+
+                // Read in a line from the console.
+                // Reference: https://msdn.microsoft.com/en-us/library/system.console.readline(v=vs.110).aspx
+                string input = Console.ReadLine();
+
+
+                // Parse the input string to an integer inside a try-catch block.
+                // In the catch block, output the exception message and immediately return the initial, default value of n.
+                // Reference: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/types/how-to-convert-a-string-to-a-number
+                try
+                {
+                    // Try to convert the input string to an integer.
+                    n = Int32.Parse(input);
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e.Message);   // Output an error message if parsing cannot be done.
+                    return n;   // Return the value n = -1, to signal an error.
+                }
+
+                // If n is negative, tell the user to enter a non-negative value.
+                if (n < minVal)
+                {
+                    // Reference: https://msdn.microsoft.com/en-us/library/system.console.write(v=vs.110).aspx
+                    Console.WriteLine("The entered value is less than {0}.  Enter a larger value.", minVal);
+                }
+                else if (maxVal != 0 && n > maxVal)
+                {
+                    // An upper limit is imposed and the user's input is greater than the upper limit.
+                    Console.WriteLine("The entered value is greater than {0}.  Enter a smaller value.", maxVal);
+                }
+
+            } while (n < minVal || (maxVal !=0 && n > maxVal));
+
+
+            return n;   // Return the non-negative input integer n.
+        }
+
+
+        public static void showResult(int n, int b, string s)
+        {
+            // Displays on the console the value of the inputs n, b, s.
+
+            Console.WriteLine("The value of {0} in base {1} is {2}.", n, b, s);
+        }
+
+
+        public static void showError()
+        {
+            Console.WriteLine("Error: could not resolve user's input.");
+        }
+
+
+        public static void waitForKeypress()
+        {
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ IntToBaseString/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntToBaseString
+{
+    class Program
+    {
+        // Array of single character strings.
+        // Use base 10 integer as an index, get the corresponding character representing that number
+        // in the selected base, up to base 36.
+        static string[] sChar = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                                 "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                                 "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+
+
+        static void Main(string[] args)
+        {
+            // Creates a string that represents an input non-negative base-10 integer in another
+            // base, which is specified by the user.
+            // For example, if the input integer is 4 and the base is 2, then the string that is 
+            // created would be "100".
+
+            // Get the number from the user.
+            int n = ConsoleIO.getNumber();
+
+            // Get the base.  Must be no less than 2 and no greater than 36.
+            int b = ConsoleIO.getNumber(2, 36);
+
+
+            // Create the string representing the integer n, in base b.
+            string s = convertIntToBaseString(n, b);
+
+
+            // Display the result on the console.
+            ConsoleIO.showResult(n, b, s);
+
+            ConsoleIO.waitForKeypress();
+        }
+
+
+        static string convertIntToBaseString(int n, int b)
+        {
+            // Converts the input integer n to a string,
+            // representing its value in base b.
+            // For example, if n = 3 and b = 2, this function returns "11".
+
+            // Successive digits are calculated by calling this function recursively.
+
+
+            string s = "";  // Initialize the return string.
+
+            // First, check if n < b.  
+            if (n < b)
+            {
+                // In this case, simply return n as a string.
+                s = sChar[n];
+            }
+            else
+            {
+                // Find the remainder, when you divide n by b.
+                // Example: n = 157, b = 10: remainder = 7.
+                int remainder = 0;  // create the remainder variable.
+                // Add code here to calculate the value of the remainder.
+                // Reference: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/modulus-operator
+                //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+                // <-------------------------------------------------------------------------------------- Add one line of code here
+                //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+
+                // Set the output to the string version of the remainder.
+                // Example: remainder = 7: endOfString = "7".
+                string endOfString = sChar[remainder];
+
+                // Calculate the reduced number to (n - remainder) / b.
+                // Example: n = 157, b = 10, remainder = 7: reducedNumber = (157 - 7) / 10 = 15.
+                int reducedNumber = (n - remainder) / b;
+
+
+
+                // Get the string representation of this reduced number.
+                // Example: reducedNumber = 15, b = 10: startOfString = "15".
+                // Note: startOfString is obtained by calling this function, convertIntToBaseString, recursively.
+                string startOfString = ""; // Create the startOfString variable.
+                // Add code here to obtain startOfString by calling this function recursively.
+                // Reference: Quick Calculation #2: Factorial by Recursion.
+                //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+                // <-------------------------------------------------------------------------------------- Add one line of code here
+                //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+
+                // Append the rest of the string and the remainder string as the output.
+                // Example: startOfString = "15", endOfString = "7": s = "157"
+                s = startOfString + endOfString;
+            }
+
+            return s;
+        }
+
+    }
+}
